5204fa35aa1c1edd6bfd0415b0cb7676dc78d53f
sonaly-mehr <sonaly.mehr@gmail.com>
2021-09-24 00:45:06 +0600

    second-commit

--- src/Home/Home/Home.js
+++ src/Home/Home/Home.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import './Home.css';
 import About from '../About/About';
-import NavBar from '../NavBar/NavBar';
-// import Sliders from '../Sliders/Sliders';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
